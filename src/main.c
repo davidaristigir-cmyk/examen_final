8ec6503a2440b8ebdefe44dbd80481fe19d52090
David Aristizabal <david.aristizabal28@eia.edu.co>
2026-05-31 15:33:47 -0500

    Plantilla limpia y neutra para el examen

--- src/main.c
+++ src/main.c
@@ -2,10 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void app_main(void)   // ← punto de entrada OBLIGATORIO en ESP-IDF
+void app_main(void)
 {
-    while (1) {
-        // tu código aquí
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    
 }
